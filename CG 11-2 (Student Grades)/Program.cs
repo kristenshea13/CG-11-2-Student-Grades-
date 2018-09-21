8ab62fa447e5823e4287809219f07e80f100c4dc
Kristen Shea <kstar61@live.com>
2018-09-20 21:59:20 -0500

    *

--- CG 11-2 (Student Grades)/Program.cs
+++ CG 11-2 (Student Grades)/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CG_11_2__Student_Grades_
 {
@@ -6,7 +8,31 @@ namespace CG_11_2__Student_Grades_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            //Using the text file studentdata.txt write a program that calculates the average grade for 
+            //each student, and print out the student’s name along with their average grade.Be sure to use a Student class.
+
+            string[] students = File.ReadAllLines(@"C:\Users\kstar\source\repos\CG 11-2 (Student Grades)\CG 11-2 (Student Grades)\studentdata.txt");
+
+            List<Student> studentData = new List<Student>();
+
+            foreach (string item in students)
+            {
+                studentData.Add(new Student(item));
+
+            }
+
+            Console.WriteLine("Student average grades: ");
+
+            foreach (Student student in studentData)
+            {
+
+                Console.WriteLine($"{student.StudentNameCap()}: {student.AverageGrade()}");
+            }
+
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ CG 11-2 (Student Grades)/Student.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CG_11_2__Student_Grades_
+{
+    class Student
+    {
+        public string Name { get; set; }
+
+        public List<int> Scores { get; set; }
+
+        public Student(string studentData)
+        {
+            string[] studentdataSplit = studentData.Split("; ");
+            Scores = new List<int>();
+
+            for (int i = 0; i < studentdataSplit.Length; i++)
+            {
+                if (i == 0)
+                {
+                    Name = studentdataSplit[i];
+                }
+                else
+                {
+                    int score = int.Parse(studentdataSplit[i]);
+                    Scores.Add(score);
+                }
+
+            }
+
+        }
+
+
+        public decimal AverageGrade()
+        {
+            int totalScore = 0;
+            foreach (int score in Scores)
+            {
+                totalScore += score;
+            }
+            return totalScore / Scores.Count;
+
+        }
+
+        public string StudentNameCap()
+        {
+            return char.ToUpper(this.Name[0]) + this.Name.Substring(1);
+
+        }
+
+    }
+}
